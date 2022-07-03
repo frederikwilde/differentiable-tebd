e7ac8387b979279523aa81c2025dd846dd07f69f
Frederik Wilde <wilde.physics@gmail.com>
2022-07-03 18:49:58 +0200

    Added custom derivative for SVD to avoid degeneracy issues.

--- differentiable_tebd/mps_utils.py
+++ differentiable_tebd/mps_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import jax
+from jax import custom_jvp
 from jax import numpy as jnp
 from . import COMPLEX_TYPE
 
@@ -102,11 +103,54 @@ def expm(coeff, hmat):
     e, u = jnp.linalg.eigh(hmat)
     return u.dot(jnp.diag(jnp.exp(coeff * e))).dot(u.transpose().conj())
 
+@custom_jvp
+def svd(a):
+    return jax.scipy.linalg.svd(a, full_matrices=False, compute_uv=True)
+
+def _T(x): return jnp.swapaxes(x, -1, -2)
+def _H(x): return jnp.conj(_T(x))
+
+@svd.defjvp
+def svd_jvp_rule(primals, tangents):
+    A, = primals
+    dA, = tangents
+    U, s, Vt = svd(A)
+
+    Ut, V = _H(U), _H(Vt)
+    s_dim = s[..., None, :]
+    dS = jnp.matmul(jnp.matmul(Ut, dA), V)
+    ds = jnp.real(jnp.diagonal(dS, 0, -2, -1))
+
+    s_diffs = (s_dim + _T(s_dim)) * (s_dim - _T(s_dim))
+    s_diffs_zeros = jnp.ones((), dtype=A.dtype) * (s_diffs == 0.)  # is 1. where s_diffs is 0. and is 0. everywhere else
+    # s_diffs_zeros = lax.expand_dims(s_diffs_zeros, range(s_diffs.ndim - 2))
+    F = 1 / (s_diffs + s_diffs_zeros) - s_diffs_zeros
+    dSS = s_dim * dS  # dS.dot(jnp.diag(s))
+    SdS = _T(s_dim) * dS  # jnp.diag(s).dot(dS)
+
+    s_zeros = jnp.ones((), dtype=A.dtype) * (s == 0.)
+    s_inv = 1 / (s + s_zeros) - s_zeros
+    s_inv_mat = jnp.vectorize(jnp.diag, signature='(k)->(k,k)')(s_inv)
+    dUdV_diag = .5 * (dS - _H(dS)) * s_inv_mat
+    dU = jnp.matmul(U, F * (dSS + _H(dSS)) + dUdV_diag)
+    dV = jnp.matmul(V, F * (SdS + _H(SdS)))
+
+    m, n = A.shape[-2:]
+    if m > n:
+        I = jax.lax.expand_dims(jnp.eye(m, dtype=A.dtype), range(U.ndim - 2))
+        dU = dU + jnp.matmul(I - jnp.matmul(U, Ut), jnp.matmul(dA, V)) / s_dim
+    if n > m:
+        I = jax.lax.expand_dims(jnp.eye(n, dtype=A.dtype), range(V.ndim - 2))
+        dV = dV + jnp.matmul(I - jnp.matmul(V, Vt), jnp.matmul(_H(dA), U)) / s_dim
+
+    return (s, U, Vt), (ds, dU, _H(dV))
+
+
 def contract_and_split(n1, n2, gate):
     chi = n1.shape[2]
     n = jnp.tensordot(n1, n2, axes=(2, 0))
     c = jnp.tensordot(n, gate, axes=((1,2), (2,3))).transpose((0, 2, 3, 1))
-    u, s, v =  jax.scipy.linalg.svd(c.reshape(2*chi, 2*chi), full_matrices=False)
+    u, s, v = jax.scipy.linalg.svd(c.reshape(2*chi, 2*chi), compute_uv=True, full_matrices=False)
     # FIXME: derivative at zero
     s_sqrt = jnp.sqrt(s[:chi])
     truncation_error_squared = jnp.sum(s[chi:] ** 2)
